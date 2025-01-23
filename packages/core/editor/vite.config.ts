f6ce22e4e13d366adde703588698cd8c28947086
plum <40649341+plum-k@users.noreply.github.com>
2025-01-24 02:10:18 +0800

    chore(editer): 升级 tailwindcss 到4.0.0

--- packages/core/editor/vite.config.ts
+++ packages/core/editor/vite.config.ts
@@ -1,7 +1,7 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import {viteStaticCopy} from "vite-plugin-static-copy";
-
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
     server: {
         host: '0.0.0.0',
@@ -9,6 +9,7 @@ export default defineConfig({
     },
     plugins: [
         react(),
+        tailwindcss(),
         viteStaticCopy({
             targets: [
                 {
